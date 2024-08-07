e9dcf6c5fb4c0cd4c679a283eb66ffb3acdb4ed4
gopikabhaskaran26 <153718647+gopikabhaskaran26@users.noreply.github.com>
2024-08-07 10:29:36 +0530

    Delete backend/demo directory

--- backend/demo/src/main/java/com/example/demo/repository/AssignmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.Model.Assignment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
-}
